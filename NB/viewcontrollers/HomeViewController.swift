87ce7057ad04af46e2a34d0daaf978f952e99d8d
Bhanu Pratap Singh Thapliyal <bpst98@gmail.com>
2019-09-11 23:49:58 +0530

    Home Viewcontroller added yet to be customised

---
+++ NB/viewcontrollers/HomeViewController.swift
@@ -0,0 +1,28 @@
+//  HomeViewController.swift
+//NB
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var ProfileView: UIView!
+    @IBOutlet weak var ProfileImageView: UIImageView!
+    @IBOutlet weak var AboutUsView: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
