5498d548b3c5586f3b255b91f7780900e19493ba
Bhanu Pratap Singh Thapliyal <bpst98@gmail.com>
2019-09-17 23:59:52 +0530

    activated constraints

--- NB/viewcontrollers/OnBoardingModelViewController.swift
+++ NB/viewcontrollers/OnBoardingModelViewController.swift
@@ -7,18 +7,27 @@ class OnBoardingModelViewController: UIViewController {
     @IBOutlet weak var scrollView : UIScrollView!
     
     var Pages = [OnBoardingViewController]()
+    var views = [String: UIView]()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
     
         
-        let scrollPage1 = createNewOnboardingscroll(backgroundImageName: "1", text: "Aye Page1 ")
-        let scrollPage2 = createNewOnboardingscroll(backgroundImageName: "2", text: "Aye Page2 ")
-        let scrollPage3 = createNewOnboardingscroll(backgroundImageName: "3", text: "Aye Page3 ")
-        let scrollPage4 = createNewOnboardingscroll(backgroundImageName: "4", text: "Aye Page4 ")
+        let scrollPage1 = createNewOnboardingscroll(backgroundImageName: "AppIcon", text: "Aye Page1 ")
+        let scrollPage2 = createNewOnboardingscroll(backgroundImageName: "AppIcon", text: "Aye Page2 ")
+        let scrollPage3 = createNewOnboardingscroll(backgroundImageName: "AppIcon", text: "Aye Page3 ")
+        let scrollPage4 = createNewOnboardingscroll(backgroundImageName: "AppIcon", text: "Aye Page4 ")
         Pages = [scrollPage1,scrollPage2,scrollPage3,scrollPage4]
         
+        views = ["scrollPage1":scrollPage1.view,
+                 "scrollPage2":scrollPage2.view,
+                 "scrollPage3":scrollPage3.view,
+                 "scrollPage4":scrollPage4.view]
+        
+        let vConstraints  = NSLayoutConstraint.constraints(withVisualFormat: "V:|[scrollPage1(==view)]!", options: [], metrics: nil, views: views)
+        let hConstraints  = NSLayoutConstraint.constraints(withVisualFormat: "H:|[scrollPage1(==view)][scrollPage2(==view)][scrollPage3(==view)][scrollPage4(==view)]!", options: [.alignAllTop,.alignAllBottom], metrics: nil, views: views)
+        NSLayoutConstraint.activate(vConstraints+hConstraints)
     }
     
     
