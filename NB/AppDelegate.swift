7f801cf4abbaedd894c511a7a353d9493a407b39
Bhanu Pratap Singh Thapliyal <bpst98@gmail.com>
2019-09-19 23:02:43 +0530

    removed onboarding crash

--- NB/AppDelegate.swift
+++ NB/AppDelegate.swift
@@ -8,8 +8,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        GIDSignIn.sharedInstance()?.clientID = "c18219731635-ek0m6uf3salr4o3fb6mc8jpige5609vi.apps.googleusercontent.com"
+//        // Override point for customization after application launch.
+//        GIDSignIn.sharedInstance()?.clientID = "c18219731635-ek0m6uf3salr4o3fb6mc8jpige5609vi.apps.googleusercontent.com"
         return true
     }
 
