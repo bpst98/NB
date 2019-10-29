cea86be49d26f5f23c5bfac58684659297f8e328
Bhanu Pratap Singh Thapliyal <bpst98@gmail.com>
2019-10-29 11:10:39 +0530

    Data model OK

--- NB/viewcontrollers/NotesTableViewController.swift
+++ NB/viewcontrollers/NotesTableViewController.swift
@@ -7,13 +7,16 @@
 import UIKit
 
 class NotesTableViewController: UITableViewController {
+    
     let Notes1 = Notes(Author: "Bhanu", Title: "Subject Number 1", Views: 100, Date: "15/11/2018", size: 23, url: "URL")
-    let Notes2 = Notes(Author: "Aditya", Title: "Subject number 1", Views: 200, Date: "15/11/2018", size: 15, url: "URL")
+    let Notes2 = Notes(Author: "Aditya", Title: "Subject number 2", Views: 200, Date: "15/12/2018", size: 15, url: "URL")
     var NotesArray = [Notes]()
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotesArray.append(Notes1)
+        NotesArray.append(Notes2)
 
     }
     
@@ -28,14 +31,18 @@ class NotesTableViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 2
+        return NotesArray.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! NotesTableViewCell
         
-        
+        cell.AuthorLabel.text = NotesArray[indexPath.row].Author
+        cell.DownloadSizeLabel.text = String(NotesArray[indexPath.row].size)
+        cell.NotesFileTitle.text = NotesArray[indexPath.row].Title
+        cell.NumberOfDownloadsLabel.text = String(NotesArray[indexPath.row].Views)
+        cell.UploadDateLabel.text = NotesArray[indexPath.row].Date
 
         return cell
     }
